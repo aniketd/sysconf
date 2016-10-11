3de5904c1274cc91e0eff8169a998e9dabe9cd99
Aniket Deshpande <anicake@gmail.com>
2016-10-11 11:55:29 +0530

    added systemd for emacs; setup.sh for overall. to be maintained.

--- setup.sh
+++ setup.sh
@@ -1,2 +1,34 @@
-sudo pacman -S git tmux gdm
+sudo pacman -S --noconfirm git tmux gdm emacs neovim xclip fish
+
+# setup git
+git config --global user.name "Aniket Deshpande"
+git config --global user.email "anicake@gmail.com"
+git config --global core.editor nvim
+git config --global core.autocrlf true
+git config --global credential.helper 'cache --timeout=3600'
+
+# clone sysconf
+mkdir ~/Code
+cd ~/Code
+git clone https://github.com/aniketd/sysconf.git
+
+# tmux
+cd ~
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+ln -s Code/sysconf/tmux.conf .tmux.conf
+echo "TMUX: `tmux source ~/.tmux.conf` and then PREFIX-I to install plugins."
+
+# oh my fish # TODO
+#curl -L http://get.oh-my.fish | fish # TODO
+
+# spacemacs
+## emacs
+mkdir -p ~/.config/systemd/user/
+cp ~/Code/sysconf/systemd.emacs.service ~/.config/systemd/user/emacs.service
+systemctl --user enable emacs.service
+systemctl --user start emacs.service
+
+# neovim
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+echo "NEOVIM: enter and type :PlugInstall!"
