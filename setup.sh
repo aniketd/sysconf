8282164c1a519d6d414ea4206bba9d34c7fe0179
Aniket Deshpande <anicake@gmail.com>
2016-12-30 18:58:12 +0530

    files added

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 sudo pacman -S --noconfirm git tmux gdm emacs neovim xclip xsel fish
-sudo yaourt -S aura-bin --noconfirm
-sudo aura -A ttf-fantasque-sans tor-browser-en --noconfirm
+sudo pacman -S --noconfirm xmonad xmonad-contrib xmobar stalonetray dmenu scrot cabal-install xcompmgr feh
+sudo yaourt -S --noconfirm aura-bin
+sudo aura -A --noconfirm ttf-fantasque-sans
+sudo aura -A --noconfirm tor-browser-en
+sudo aura -A --noconfirm ttf-fantasque-sans
 
 # setup git
 git config --global user.name "Aniket Deshpande"
@@ -9,10 +12,18 @@ git config --global core.editor nvim
 git config --global core.autocrlf input
 git config --global credential.helper 'cache --timeout=3600'
 
-# clone sysconf
 mkdir ~/Code
-cd ~/Code
+
+# clone sysconf
+pushd ~/Code
 git clone https://github.com/aniketd/sysconf.git
+popd
+
+# xmonad
+cd
+git clone https://github.com/aniketd/xmonad-config.git .xmonad
+ln -s .xinitrc .xmonad/xmonad-session-rc
+ln -s .xsessionrc .xmonad/xmonad-session-rc
 
 # tmux
 cd ~
@@ -26,8 +37,8 @@ curl -L http://get.oh-my.fish | fish # TODO
 # spacemacs
 mkdir -p ~/.config/systemd/user/
 cp ~/Code/sysconf/systemd.emacs.service ~/.config/systemd/user/emacs.service
-systemctl --user enable emacs.service
-systemctl --user start emacs.service
+# systemctl --user enable emacs.service
+# systemctl --user start emacs.service
 
 # neovim
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
