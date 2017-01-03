44ff1049d601f2b2bc83b8c65ce4df6fd20726b9
Aniket Deshpande <anicake@gmail.com>
2017-01-04 01:43:25 +0530

    working dual monitors finally!.. now to make others

--- setup.sh
+++ setup.sh
@@ -1,9 +1,13 @@
-sudo pacman -S --noconfirm git tmux gdm emacs neovim xclip xsel fish
-sudo pacman -S --noconfirm xmonad xmonad-contrib xmobar stalonetray dmenu scrot cabal-install xcompmgr feh
+# update this sh*t
+sudo pacman -Syyu --noconfirm
+
+# basic setup
+sudo pacman -S --noconfirm git tmux gdm emacs neovim xclip xsel fish inotify-tools
 sudo yaourt -S --noconfirm aura-bin
 sudo aura -A --noconfirm ttf-fantasque-sans
 sudo aura -A --noconfirm tor-browser-en
-sudo aura -A --noconfirm ttf-fantasque-sans
+sudo aura -A --noconfirm spideroak-one encryptr
+sudo aura -A --noconfirm slack-desktop
 
 # setup git
 git config --global user.name "Aniket Deshpande"
@@ -15,21 +19,30 @@ git config --global credential.helper 'cache --timeout=3600'
 mkdir ~/Code
 
 # clone sysconf
-pushd ~/Code
+cd ~/Code
 git clone https://github.com/aniketd/sysconf.git
-popd
+cd
+
+# xorg.conf for xmonad
+sudo ln -s ~/Code/sysconf/xorg.conf /etc/X11/xorg.conf
+ln -s ~/Code/sysconf/blackhole.png ~/Pictures/blackhole.png
 
 # xmonad
+sudo pacman -S --noconfirm xmonad xmonad-contrib xmobar stalonetray dmenu scrot cabal-install xcompmgr feh slim
+sudo cabal-update
+sudo cabal-install --global yeganesh
 cd
+mv .xmonad .xmonad.orig
 git clone https://github.com/aniketd/xmonad-config.git .xmonad
 ln -s .xinitrc .xmonad/xmonad-session-rc
 ln -s .xsessionrc .xmonad/xmonad-session-rc
+ln -s .xsession .xmonad/xmonad-session-rc
 
 # tmux
 cd ~
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 ln -s Code/sysconf/tmux.conf .tmux.conf
-echo "TMUX: `tmux source ~/.tmux.conf` and then PREFIX-I to install plugins."
+echo "TMUX: `tmux source ~/.tmux.conf` and then PREFIX-I (^b-I) to install plugins."
 
 # oh my fish # TODO
 curl -L http://get.oh-my.fish | fish # TODO
@@ -39,6 +52,8 @@ mkdir -p ~/.config/systemd/user/
 cp ~/Code/sysconf/systemd.emacs.service ~/.config/systemd/user/emacs.service
 # systemctl --user enable emacs.service
 # systemctl --user start emacs.service
+git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+ln -s ~/Code/sysconf/spacemacs ~/.spacemacs
 
 # neovim
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
